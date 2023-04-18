bcd390abe46fe3fe224b6267caf147f0045fc153
Kevin David <kevincarld@gmail.com>
2023-04-18 20:09:23 +1000

    Fixed origin to brandedcontent

--- src/pages/api/formstack.js
+++ src/pages/api/formstack.js
@@ -5,7 +5,7 @@ import Cors from 'cors'
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ['POST'],
-  origin: '*',
+  origin: ['brandedcontent.nine.com.au'],
 })
 
 // Helper method to wait for a middleware to execute before continuing
