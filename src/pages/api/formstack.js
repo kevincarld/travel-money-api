a4f26231b24575ba1c9ccf849d45646272ac0668
Kevin David <kevincarld@gmail.com>
2023-04-18 20:03:36 +1000

    Fixed logic in api

--- src/pages/api/formstack.js
+++ src/pages/api/formstack.js
@@ -30,45 +30,28 @@ export default async function handler(req, res) {
 
   // Extract the email and captcha code from the request body
   if (method === "POST") {
-    console.log(body)
-  }
-
-  try {
-    // Ping the google recaptcha verify API to verify the captcha code you received
     const config = {
       method: "POST",
       headers: {
-        "Access-Control-Allow-Origin": '*',
+        accept: 'application/json',
         "Content-Type": "application/json; charset=utf-8",
         "Authorization": "Bearer 154935645fdd40d88096faa4eeaf8999"
       },
-      // mode: 'no-cors',
-      body: JSON.stringify({
-        "field_143047240": "2fromnextjs",
-        "field_143047326": "s",
-        "field_143048112": "s",
-        "field_143048125": "s",
-        "field_143048108": "s",
-        "field_143048126": "s",
-        "field_143048302": true,
-        "field_143048305": true,
-        "meta": {
-          "createdOn": "2023-04-18T00:58:38.318Z"
-        }
-      }),
+      body: JSON.stringify(body),
     };
-    // fetch('https://competition.nine.com.au/enter-phase/436', config)
-        fetch('https://www.formstack.com/api/v2/form/5252692/submission.json', config)
-          .then((response) => response.json())
-          .then((json) => {
-            console.log('data', json);
+    //await fetch('https://competition.nine.com.au/enter-phase/436', config)
+    await fetch('https://www.formstack.com/api/v2/form/5252692/submission.json', config)
+      .then(response => response.json())
+      .then(response => {
+        // console.log(response);
+        res.status(200).json({...response, success: true});
+      })
+      .catch(err => console.error(err));
+  } else {
 
-          })
-  } catch(err) {
-    // console.log(error);
-    return res.status(422).json({ message: "Something went wrong" });
+    // Return 404 if someone pings the API with a method other than
+    // POST
+    return res.status(404).send("Invalid request sir");
   }
-  // Return 404 if someone pings the API with a method other than
-  // POST
-  return res.status(404).send("Not found");
+
 }
